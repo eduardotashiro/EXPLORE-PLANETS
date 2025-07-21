392b8db0abb74eef243e0258b5fa94149738ba94
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-07-20 21:15:15 -0300

    projeto finalizado?

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+//-- Seleciona todos os elementos com a classe "panel"
 const panels = document.querySelectorAll('.panel')
 
+//-- Para cada painel encontrado...
 panels.forEach((panel) => {
-   panel.addEventListener('click', () => {
-    removeActiveClasses()
-    panel.classList.add('active')
+    //-- Adiciona um "escutador" de clique
+    panel.addEventListener('click', () => {
+    removeActiveClasses() //-- Remove a classe "active" de todos os painéis
+    panel.classList.add('active') //-- Adiciona a classe "active" apenas ao clicado
    })
 
 })
 
+//-- Função que remove a classe "active" de todos os painéis
 function removeActiveClasses(){
     panels.forEach(panel =>{
         panel.classList.remove('active')
